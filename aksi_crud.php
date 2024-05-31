14f757b827db7d83ca731485b37a05299bb6958f
dayzign <dayzign@yahoo.com>
2024-05-31 15:58:39 +0700

    error syntax

--- aksi_crud.php
+++ aksi_crud.php
@@ -4,28 +4,27 @@
 include "koneksi.php";
 
 // Uji jika tombol simpan di klik
-if(isset($_POST['bsimpan'])) {
+if (isset($_POST['bsimpan'])) {
 
     //persiapan simpan data baru
     $simpan = mysqli_query($koneksi, "INSERT INTO tmhs (nim, nama, alamat, lulusan)
-                                        VALUES ('$_POST[tnim],
-                                                '$_POST[tnama],
-                                                '$_POST[talamat],
-                                                '$_POST[tlulusan]')");
+                                        VALUES ('" . $_POST['tnim'] . "',
+                                                '" . $_POST['tnama'] . "',
+                                                '" . $_POST['talamat'] . "',
+                                                '" . $_POST['tlulusan'] . "')");
 
     // Jika simpan sukses
-    if ($simpan){
+    if ($simpan) {
         echo "<script>
                 alert('Simpan data Sukses bro!');
                 document.location='index.php';
-                </script>";
-    } else {               
-        echo "<script>       
+              </script>";
+    } else {
+        echo "<script>
                 alert('Simpan data Gagal coy!');
                 document.location='index.php';
-                </script>;
-            }
-
+              </script>";
     }
+}
 
 ?>
