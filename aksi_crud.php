5197b76bba280f2f94a7defc30e69ecc6398eaca
dayzign <dayzign@yahoo.com>
2024-05-31 15:47:51 +0700

    test ngoding tambah data

--- aksi_crud.php
+++ aksi_crud.php
@@ -1,7 +1,36 @@
 <?php
 
+//panggil koneksi database
+include "koneksi.php";
 
-// test
+// Uji jika tombol simpan di klik
+if(isset($_POST['bsimpan'])) {
+
+    //persiapan simpan data baru
+    $simpan = mysqli_query($koneksi, "INSERT INTO tmhs (nim, nama, alamat, lulusan)
+                                        VALUES ('$_POST[tnim],
+                                                '$_POST[tnama],
+                                                '$_POST[talamat],
+                                                '$_POST[tlulusan]')");
+
+    // Jika simpan sukses
+    if ($simpan){
+        echo "<script>
+                alert('Simpan data Sukses bro!');
+                document.location='index.php';
+                </script>";
+    } else {               
+        echo "<script>       
+                alert('Simpan data Gagal coy!');
+                document.location='index.php';
+                </script>;
+            }
+
+    }
+
+}
+
+
 
 
 ?>
